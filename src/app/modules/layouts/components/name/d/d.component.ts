4449148a59b9bafe5ffed83dbabfb4fcff3cc735
Ram Manohar <rammanohar@inncrewin.com>
2024-06-06 10:58:13 +0530

    Code updated for the deployment id:

---
+++ src/app/modules/layouts/components/name/d/d.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-d',
+  templateUrl: './d.component.html',
+  styleUrls: ['./d.component.scss']
+})
+
+export class DComponent {
+    
+}
